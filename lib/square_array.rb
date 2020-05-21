5e53e2d669172b2dc8bd23a1f536d58572cd2983
Pramod Thomas <pmod23@gmail.com>
2020-05-21 16:01:22 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+  while counter < array.size()
+  array[counter] = array[counter] * array[counter]
+  counter = counter + 1
+end
+return array
 end
